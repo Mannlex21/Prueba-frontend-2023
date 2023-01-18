446e65a6e69a97ababaa9c28efbf91bf43ae38fa
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-17 18:35:25 -0700

    Add numbersTop function

--- 2.js
+++ 2.js
@@ -3,10 +3,14 @@
 // crea una función numbersTop para obtener los tres elementos más repetidos ordenados de forma descendente por número de repeticiones.
 
 function numbersTop(array) {
+    return array.sort((a,b) => array.filter(v => v===b).length - array.filter(v => v===a).length)
+    .filter((value, index, self) =>  {
+        return  self.indexOf(value) == index;
+    }).slice(0,3);
 }
 
 /**
  * TEST Ejercicio 2
  */
-numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1]) // [ 1, 3, 2 ]
-numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, 'a', 3]) // [ 'a', 3, 2 ]
+console.log(numbersTop([3, 3, 1, 4, 1, 3, 1, 1, 2, 2, 2, 3, 1, 3, 4, 1])) // [ 1, 3, 2 ]
+console.log(numbersTop(['a', 3, 2, 'a', 2, 3, 'a', 3, 4, 'a', 'a', 1, 'a', 2, 'a', 3])) // [ 'a', 3, 2 ]
