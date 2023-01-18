efd99fb983581accb406ee27d811b4cae2ad66c9
Mannlex Murillo <mannlexx21@gmail.com>
2023-01-17 18:35:38 -0700

    Add increasedMeasurements function

--- 3.js
+++ 3.js
@@ -4,10 +4,13 @@
 // Ejemplo: en el caso de [1, 5, 3, 5] existen 2 incremetos ( el día 2 respecto al dia 1 y el dia 4 respecto al día 3 )
 
 function increasedMeasurements(measurements) {
+    return measurements.filter((value, index, self) =>  {
+        return  self[index+1] > value;
+    }).length;
 }
 
 /**
  * TEST Ejercicio 1
  */
 const measurements = [245, 248, 259, 190, 180, 185, 191, 185, 188, 189, 204, 213, 215, 227, 222, 221, 236, 235, 236, 232, 224, 221, 228, 234, 226, 227, 228, 230, 232, 234]
-increasedMeasurements(measurements) // 19
+console.log(increasedMeasurements(measurements)) // 19
